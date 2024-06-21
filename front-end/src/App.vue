80b13dcd9789b9981caa15c1b54d72a843b6f6fc
PauloVictor <paulovictor@labtime.ufg.br>
2024-06-20 22:53:01 -0300

    feat(Projeto): Atualiza arquivos e adiciona Tailwind e Pinia no projeto

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -1,30 +1,31 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <div>{{ counterStore.name }}</div>
+  <button @click="counterStore.increment()">Incrementa</button>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+<script lang="ts">
+import { useCounterStore } from '../stores/index'
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'App',
+  components: {
+  },
+  data() {
+    return {
+      counterStore: useCounterStore()
+    }
+  },
+});
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
 }
 </style>
--- front-end/src/main.ts
+++ front-end/src/main.ts
@@ -1,5 +1,6 @@
+import { createPinia } from 'pinia'
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
+import './assets/css/main.css'
 
-createApp(App).mount('#app')
+createApp(App).use(createPinia()).mount('#app')
